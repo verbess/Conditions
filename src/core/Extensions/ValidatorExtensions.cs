e19428cf70b7e0d996abf44bf2eaa0f62800be86
Verbess <verbess@foxmail.com>
2021-03-08 22:57:05 +0800

    refactor(core): rm version 1

--- src/core/Extensions/ValidatorExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Verbess.Utils.Conditions
-{
-    /// <summary>
-    /// Extension methods for <see cref="Validator{T}"/>.
-    /// </summary>
-    public static partial class ValidatorExtensions { }
-}
